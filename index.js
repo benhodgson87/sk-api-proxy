9c0c32e812549dd28657b9d3a62f620b797bb287
Ben Hodgson <ben@someaddress.net>
2017-08-02 11:32:38 +0100

    -am

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-const port = 5555
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 
