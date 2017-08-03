ba9468883363fcd17ded852ac5aeb00a5fe69508
Ben Hodgson <ben@someaddress.net>
2017-08-03 13:16:50 +0100

    SK API doesn't like diacritics via fetch, replace with latin chars

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const jdu = require('jdu')
 const app = express()
 
 const port = process.env.PORT || 8080
@@ -10,7 +11,7 @@ const port = process.env.PORT || 8080
 app.use(bodyParser.json())
 
 const createUrl = query => {
-    const endpoint = decodeURIComponent(query)
+    const endpoint = jdu.replace(query)
     const apiUrl = 'http://api.songkick.com/api/3.0/'
     const apiKey = (endpoint.includes('?') ? `&` : `?`) + `apikey=${process.env.SK_API_KEY}`
     return `${apiUrl}${endpoint}${apiKey}`
@@ -18,10 +19,15 @@ const createUrl = query => {
 
 const request = endpoint => {
     const url = createUrl(endpoint)
-    console.log(`Received: ${endpoint} | Requesting: ${url}`)
+    console.log(`-- Requesting: ${url}`)
 
     return new Promise((resolve, reject) => {
-        fetch(url, { method: 'GET' })
+        fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                'content-type': 'application/json; charset=utf-8'
+            })
+        })
         .then(r => r.json())
         .then(data => {
             resolve(data)
@@ -32,35 +38,42 @@ const request = endpoint => {
     })
 }
 
+const respond = (res, payload) => {
+    res.set({ 'content-type': 'application/json; charset=utf-8' })
+    res.send(payload)
+}
+
 app.get('/', function (req, res) {
     const endpoint = req.query.get
+    console.log(`-- Received GET: ${endpoint}`)
 
-    if (!!req.query.get && !!process.env.SK_API_KEY) {
+    if (req.query.get && !!process.env.SK_API_KEY) {
         request(endpoint)
         .then(data => {
-            res.send(data)
+            respond(res, data)
         })
         .catch(err => {
-            res.send(err)
+            respond(res, err)
         })
     } else {
-        res.send('Please pass a get parameter containing the SK API resource you want to retrieve.')
+        respond(res, 'Please pass a get parameter containing the SK API resource you want to retrieve.')
     }
 })
 
 app.post('/', function(req, res){
     const endpoint = req.body.endpoint
+    console.log(`-- Received POST: ${endpoint}`)
 
     if (!!req.body.endpoint && !!process.env.SK_API_KEY) {
         request(endpoint)
         .then(data => {
-            res.send(data)
+            respond(res, data)
         })
         .catch(err => {
-            res.send(err)
+            respond(res, err)
         })
     } else {
-        res.send('Please post a body object containing the SK API resource you want to retrieve.')
+        respond(res, 'Please post a body object containing the SK API resource you want to retrieve.')
     }
 });
 
