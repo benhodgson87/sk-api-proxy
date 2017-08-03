25f7f67afdc44f74915eb77732c60a833b511597
Ben Hodgson <ben@someaddress.net>
2017-08-03 10:12:07 +0100

    Add post and get support, make DRYer

--- index.js
+++ index.js
@@ -9,28 +9,62 @@ const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 
+const createUrl = query => {
+    const endpoint = decodeURI(query)
+    const apiUrl = 'http://api.songkick.com/api/3.0/'
+    const apiKey = (endpoint.includes('?') ? `&` : `?`) + `apikey=${process.env.SK_API_KEY}`
+    return `${apiUrl}${endpoint}${apiKey}`
+}
+
+const request = endpoint => {
+    const url = createUrl(endpoint)
+    console.log(`Received: ${endpoint} | Requesting: ${url}`)
+
+    return new Promise((resolve, reject) => {
+        fetch(url, { method: 'GET' })
+        .then(r => r.json())
+        .then(data => {
+            resolve(data)
+        })
+        .catch(err => {
+            reject(err)
+        })
+    })
+}
+
 app.get('/', function (req, res) {
-    res.send('This is a proxy to add CORS support to the Songkick API. Please send a POST request.')
+    const endpoint = req.query.get
+
+    if (!!req.query.get && !!process.env.SK_API_KEY) {
+        request(endpoint)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    } else {
+        res.send('Please pass a get parameter containing the SK API resource you want to retrieve.')
+    }
 })
 
 app.post('/', function(req, res){
     const endpoint = req.body.endpoint
-    const apiKeyString = (endpoint.includes('?') ? `&` : `?`) + `apikey=${process.env.SK_API_KEY}`
-    const apiUrl = `http://api.songkick.com/api/3.0/${endpoint}${apiKeyString}`
 
-    console.log(`Received: ${req.body} | Requesting: ${apiUrl}`)
-
-    fetch(apiUrl, { method: 'GET' })
-    .then(resp => resp.json())
-    .then(data => {
-        res.send(data)
-    })
-    .catch(err => {
-        res.send(err)
-    })
+    if (!!req.body.endpoint && !!process.env.SK_API_KEY) {
+        request(endpoint)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    } else {
+        res.send('Please post a body object containing the SK API resource you want to retrieve.')
+    }
 });
 
 
 app.listen(port, function () {
-    console.log(`API proxy running on localhost:${port}!`)
+    console.log(`API proxy listening on port ${port}`)
 })
