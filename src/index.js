8b2845bd416e8d97f491e36198b07307e5826616
Ben Hodgson <ben@someaddress.net>
2017-08-05 16:10:32 +0100

    Use the config defined api key constants

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import Express from 'express'
 import BodyParser from 'body-parser'
 import ApiHandler from './libs/apiHandler'
+import * as config from './config'
 
 const api = new ApiHandler()
 const app = Express()
 const port = process.env.PORT || 8080
 
 const logFeedback = (req, res, next) => {
-  if (!process.env.SK_API_KEY) console.log('>> ERROR: No API Key Defined!')
+  if (!config.API_KEY) console.log('>> ERROR: No API Key Defined!')
   console.log(`>> Received ${req.method}: ${req.originalUrl}`)
   next()
 }
 
 const requestHandler = (req, res) => {
-  if (req.method === 'GET' && process.env.SK_API_KEY) {
+  if (req.method === 'GET' && config.API_KEY) {
     api.request(req.originalUrl)
       .then(data => res.send(data))
       .catch(err => res.send(err))
